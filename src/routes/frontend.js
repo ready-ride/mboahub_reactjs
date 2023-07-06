8a004c2685dfdb41669e8707e1fa233306141c0a
= <=>
2023-07-06 07:47:20 +0100

    get search input

---
+++ src/routes/frontend.js
@@ -0,0 +1,8 @@
+export let PROPERTIES_URL = '/properties';
+export const getPropertiesUrl = (listingName, location, businessType) => {
+  listingName = listingName ? `/${listingName}` : '';
+  location = location ? `/${location }` : '';
+  businessType = businessType ? `/${businessType }` : '';
+
+  return PROPERTIES_URL + `${listingName}${location}${businessType}`;
+}
